b3c6a17df847fd8e143fa340c742a5ff7f5dc3a4
Efrem Ghebre <efremg69@gmail.com>
2024-04-10 20:10:56 +0200

    edited navigation

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <ul className='nav'>
           <div className="logo">Efrem</div>
             <li>
-              <NavLink to="/" activeClassName="active">Start</NavLink>
+              <NavLink to="/home" activeClassName="active">Start</NavLink>
             </li>
             <li>
               <NavLink to="/cv" activeClassName="active">CV</NavLink>
@@ -38,7 +38,7 @@ function App() {
         </nav>
 
         <Routes>
-          <Route path="/" exact element={<Home />} />
+          <Route path="/home" exact element={<Home />} />
           <Route path="/cv" element={<CV />} />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/kontakt" element={<Kontakt />} />
